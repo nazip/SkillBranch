fcd4a5711a344325ce99078619e3342a753f18b6
nazip <nazipm@mail.ru>
2016-11-15 22:11:20 +0300

    qq

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 import express from 'express';
 import fetch from 'isomorphic-fetch';
+import mongoose from 'mongoose';
 import promise from 'bluebird';
 
 import canonize from './canonize';
+
+const pc = {"board":{"vendor":"IBM",
+           "model":"IBM-PC S-100",
+           "cpu":{"model":"80286","hz":12000},
+           "image":"http://www.s100computers.com/My%20System%20Pages/80286%20Board/Picture%20of%2080286%20V2%20BoardJPG.jpg",
+           "video":"http://www.s100computers.com/My%20System%20Pages/80286%20Board/80286-Demo3.mp4"
+         },
+"ram":{"vendor":"CTS","volume":1048576,"pins":30},
+"os":"MS-DOS 1.25",
+"floppy":0,
+"hdd":[
+  {"vendor":"Samsung","size":33554432,"volume":"C:"},
+  {"vendor":"Maxtor","size":16777216,"volume":"D:"},
+  {"vendor":"Maxtor","size":8388608,"volume":"C:"}
+],
+"monitor":null,
+"length":42,
+"height":21,
+"width":54
+}
+
 
 const app = express();
 
@@ -18,7 +40,7 @@ app.get("/", async (req, res) => {
 
     res.send("@"+(""+matches).match(reg1));
   } catch(e) {
-    return res.send(e);
+    return res.send(e); 
   }
 });
 
--- src/test.js
+++ src/test.js
@@ -11,3 +11,25 @@ urls.forEach(url => {
   const userName = canonize(url);
   console.log(userName);
 });
+
+
+mongoose.promise = promise;
+mongoose.connect('mongodb://publicdb.mgbeta.ru/nazipm_skb3');
+
+const Pet = mongoose.model('Pet', {
+  type: String,
+  name: String,
+});
+
+const Cat = new Pet({
+  type: "cat",
+  name: "shurik",
+});   
+
+Cat.save()
+.then(() => {
+  console.log("saved !!!");
+})
+.catch(() => {
+  console.log("error writting to db !!!");
+});
